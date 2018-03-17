c8130cd184179e6723863a2b98ffc1412219dab0
Masato Ito <ito-masato@plazacreate.co.jp>
2018-03-17 17:04:36 +0900

    tune the contents of contact email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
-    mail to: contact.email, subject: "#{contact.name}さんからのお問い合わせ"
+    mail to: contact.email, subject: "#{contact.name}さんからのお問い合わせを受け付けました"
   end
 end
