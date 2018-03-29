7197cd991de981b7f0bbfd63a682d7c1f05964a5
Masato Ito <ito-masato@plazacreate.co.jp>
2018-03-29 23:00:20 +0900

    remove game_relations

--- app/models/game_relation.rb
+++
@@ -1,4 +0,0 @@
-class GameRelation < ApplicationRecord
-  belongs_to :team
-  belongs_to :game_record
-end
--- test/controllers/game_relations_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class GameRelationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
