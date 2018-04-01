d88d15a03cc3bff188e3c53deb21b482bac8b011
Masato Ito <ito-masato@plazacreate.co.jp>
2018-04-01 23:21:00 +0900

    game delete test

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,18 +1,18 @@
 class Game < ApplicationRecord
   # has_one association with InningsDetail model
-  has_one :score
+  has_one :score, dependent: :destroy
   accepts_nested_attributes_for :score
 
   # belongs_to Team model
   belongs_to :team, dependent: :destroy
 
   # multiple association with BattingRecord model
-  has_many :batting_records, dependent: :destroy
-  has_many :batting_members, through: :batting_records, source: :member
+  # has_many :batting_records, dependent: :destroy
+  # has_many :batting_members, through: :batting_records, source: :member
 
   # multiple association with PitchingRecord model
-  has_many :pitching_records, dependent: :destroy
-  has_many :pitching_members, through: :pitching_records, source: :member
+  # has_many :pitching_records, dependent: :destroy
+  # has_many :pitching_members, through: :pitching_records, source: :member
 
   # validates presence team name
   validates :team_top, presence: true
