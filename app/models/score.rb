9ce7b744eb05813832d2cd34aa15d31fa067de16
Masato Ito <ito-masato@plazacreate.co.jp>
2018-04-11 21:28:34 +0900

    delete useless css

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,28 +1,3 @@
 class Score < ApplicationRecord
   belongs_to :game, dependent: :destroy
-
-  # validates :top_of_first, greater_than_or_equal_to: 0
-  # validates :top_of_second, greater_than_or_equal_to: 0
-  # validates :top_of_third, greater_than_or_equal_to: 0
-  # validates :top_of_fourth, greater_than_or_equal_to: 0
-  # validates :top_of_fifth, greater_than_or_equal_to: 0
-  # validates :top_of_sixth, greater_than_or_equal_to: 0
-  # validates :top_of_seventh, greater_than_or_equal_to: 0
-  # validates :top_of_eighth, greater_than_or_equal_to: 0
-  # validates :top_of_ninth, greater_than_or_equal_to: 0
-  # validates :top_of_tenth, greater_than_or_equal_to: 0
-  # validates :top_of_eleventh, greater_than_or_equal_to: 0
-  # validates :top_of_twelfth, greater_than_or_equal_to: 0
-  # validates :bottom_of_first, greater_than_or_equal_to: 0
-  # validates :bottom_of_second, greater_than_or_equal_to: 0
-  # validates :bottom_of_third, greater_than_or_equal_to: 0
-  # validates :bottom_of_fourth, greater_than_or_equal_to: 0
-  # validates :bottom_of_fifth, greater_than_or_equal_to: 0
-  # validates :bottom_of_sixth, greater_than_or_equal_to: 0
-  # validates :bottom_of_seventh, greater_than_or_equal_to: 0
-  # validates :bottom_of_eighth, greater_than_or_equal_to: 0
-  # validates :bottom_of_ninth, greater_than_or_equal_to: 0
-  # validates :bottom_of_tenth, greater_than_or_equal_to: 0
-  # validates :bottom_of_eleventh, greater_than_or_equal_to: 0
-  # validates :bottom_of_twelfth, greater_than_or_equal_to: 0
 end
