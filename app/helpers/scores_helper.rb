c41eb625af048176c80c4489fea74f74c7889731
Masato Ito <ito-masato@plazacreate.co.jp>
2018-04-11 19:19:35 +0900

    remove scores controller

--- app/helpers/scores_helper.rb
+++
@@ -1,9 +0,0 @@
-module ScoresHelper
-  def choose_new_or_edit_score
-    if action_name == 'new' || action_name == 'confirm'
-      scores_path
-    elsif action_name == 'edit'
-      score_path
-    end
-  end
-end
