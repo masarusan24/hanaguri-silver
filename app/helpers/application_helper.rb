04af1a475a6affb0df0536b79e62f186be8a5ac1
Masato Ito <ito-masato@plazacreate.co.jp>
2018-04-21 12:41:54 +0900

    markdown instance parameter by hash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,18 @@ module ApplicationHelper
   def markdown(text)
     unless @markdown
       renderer = Redcarpet::Render::HTML.new(filter_html: true, hard_wrap: true)
-      @markdown = Redcarpet::Markdown.new(renderer, tables: true)
+      options = {
+        autolink: true,
+        space_after_headers: true,
+        no_intra_emphasis: true,
+        fenced_code_blocks: true,
+        tables: true,
+        hard_wrap: true,
+        xhtml: true,
+        lax_html_blocks: true,
+        strikethrough: true
+      }
+      @markdown = Redcarpet::Markdown.new(renderer, options)
     end
     @markdown.render(text).html_safe
   end
