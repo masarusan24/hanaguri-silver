fc298f4b1310785967f041ad9572b7b92e6f2e3b
Masato Ito <ito-masato@plazacreate.co.jp>
2018-10-31 12:06:05 +0900

    add member model spec

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,7 +3,7 @@ class Member < ApplicationRecord
   has_many :batting_records, dependent: :destroy
   has_many :pitching_records, dependent: :destroy
   has_many :game_records, through: :batting_records, source: :game_record
-  validates :name, presence: true, length: { maximum: 30 }
+  validates :name, presence: true, length: { maximum: 30 }, uniqueness: true
   validates :full_name, presence: true, length: { maximum: 30 }
   mount_uploader :image, ImageUploader
 end
---
+++ spec/models/member_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe Member, type: :model do
+  before do
+    @team = Team.create(
+      team_name:       'Hanaguri Silver',
+      team_short_name: 'Silver'
+    )
+
+    @member = @team.members.create(
+      name:      'masarusan24',
+      full_name: 'Masato Ito'
+    )
+  end
+
+  # 名前(登録名)とフルネームがあれば有効
+  it 'is valid with name and full_name' do
+    member = @team.members.build(
+      name:      'ito_ma',
+      full_name: 'Masato Ito'
+    )
+    expect(member).to be_valid
+  end
+
+  # 名前(登録名)が30文字を超えたら無効
+  it 'is invalid with name over 30 charactors' do
+    member = @team.members.build(
+      name:      'a' * 31,
+      full_name: 'hoge fuga'
+    )
+    expect(member).to be_invalid
+  end
+
+  # フルネームが30文字を超えたら無効
+  it 'is invalid with full_name over 30 charactors' do
+    member = @team.members.build(
+      name:      'ito_ma',
+      full_name: 'a' * 31
+    )
+    expect(member).to be_invalid
+  end
+
+  # 名前(登録名)が被っていたら無効
+  it 'does not allow duplicate member`s name' do
+    member = @team.members.build(
+      name:      @member.name,
+      full_name: 'hoge fuga'
+    )
+    member.valid?
+    expect(member.errors[:name]).to include('はすでに存在します')
+  end
+
+  # フルネームが被っていた場合は有効(同姓同名を考慮)
+  it 'allows duplicate member`s full_name' do
+    member = @team.members.build(
+      name:      'ito_ma',
+      full_name: @member.full_name
+    )
+    expect(member).to be_valid
+  end
+end
