e10506c15e6411abfc52f38d435dad080a6e09f4
Masato Ito <ito-masato@plazacreate.co.jp>
2018-11-06 13:57:11 +0900

    refactoring member_spec by using FactoryBot

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -12,47 +12,32 @@ RSpec.describe Member, type: :model do
 
   # 名前(登録名)とフルネームがあれば有効
   it 'is valid with name and full_name' do
-    member = @team.members.build(
-      name:      'ito_ma',
-      full_name: 'Masato Ito'
-    )
+    member = FactoryBot.build(:member)
     expect(member).to be_valid
   end
 
   # 名前(登録名)が30文字を超えたら無効
   it 'is invalid with name over 30 charactors' do
-    member = @team.members.build(
-      name:      'a' * 31,
-      full_name: 'hoge fuga'
-    )
+    member = FactoryBot.build(:member, name: 'a' * 31)
     expect(member).to be_invalid
   end
 
   # フルネームが30文字を超えたら無効
   it 'is invalid with full_name over 30 charactors' do
-    member = @team.members.build(
-      name:      'ito_ma',
-      full_name: 'a' * 31
-    )
+    member = FactoryBot.build(:member, full_name: 'a' * 31)
     expect(member).to be_invalid
   end
 
   # 名前(登録名)が被っていたら無効
   it 'does not allow duplicate member`s name' do
-    member = @team.members.build(
-      name:      @member.name,
-      full_name: 'hoge fuga'
-    )
+    member = FactoryBot.build(:member, name: @member.name)
     member.valid?
     expect(member.errors[:name]).to include('はすでに存在します')
   end
 
   # フルネームが被っていた場合は有効(同姓同名を考慮)
   it 'allows duplicate member`s full_name' do
-    member = @team.members.build(
-      name:      'ito_ma',
-      full_name: @member.full_name
-    )
+    member = FactoryBot.build(:member, full_name: @member.full_name)
     expect(member).to be_valid
   end
 end
